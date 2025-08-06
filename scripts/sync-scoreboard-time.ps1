f24b114e1b2e3375e530070d040c34e84ab2be10
Daniel Errington <derrington@gmail.com>
2025-08-06 18:42:41 +0100

    Remove unnecessary line from time sync file

--- scripts/sync-scoreboard-time.ps1
+++ scripts/sync-scoreboard-time.ps1
@@ -1,6 +1,6 @@
 $date = Get-Date -Format "d MMM yyyy hh:mm:ss"
 Write-Output "Setting date to `"$date`""
 $command = "sudo date --set \`"$date\`""
-Write-Output statsaregood | clip
+
 ssh pi@scoreboard1 "$command"
 ssh pi@scoreboard2 "$command"
